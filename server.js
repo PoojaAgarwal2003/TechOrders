7fe7719abfb89c1f0df0cd89e627e98d626b4a8f
PoojaAgarwal2003 <113182679+PoojaAgarwal2003@users.noreply.github.com>
2023-12-23 01:06:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import cors from 'cors';
 import path from 'path';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
+import {fileURLToPath} from 'url';
 //config env
 dotenv.config()
 
@@ -18,6 +19,8 @@ const app=express();
 connectDB();
 
 //middlewares
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'))
